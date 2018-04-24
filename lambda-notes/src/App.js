e20f4cd1125526bb9bc81135c79d4f1f8fd9c850
jaivonmassena <jaivonmassena@gmail.com>
2018-04-24 13:20:00 -0400

    buttons and sidebar added, need to get rid of scroll bar

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import main from './Components/Main';
 
+const Result =() =>{
+  return(<div className="Sidebar">
+          <h1>Lambda Notes</h1>
+
+          <button>1</button>
+          <button>2</button>
+    
+  </div>);
+}
+const List =() =>{return (<div className="MainContent"></div>);}
 class App extends Component {
+
+
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Result />
+      <List />
       </div>
     );
   }
